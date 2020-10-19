8dc8b547df41019e3b058c10d9aee6d2839d99ea
1ahmadsam <samiahmad360@hotmail.com>
2020-10-19 06:48:07 -0400

    added styles

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chat.css';
 
 const Chat = () => {
+  const [input, setInput] = useState('');
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className='chat'>
       <div className='chat__header'>
@@ -9,6 +14,18 @@ const Chat = () => {
           To: <span className='chat__name'>Channel name</span>{' '}
         </h4>
         <strong>Details</strong>
+      </div>
+
+      <div className='chat__input'>
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder='iMessage'
+            type='text'
+          />
+          <button onClick={sendMessage}>Send Message</button>
+        </form>
       </div>
     </div>
   );
