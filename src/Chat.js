ebf1be8f75e0acfaf3d36230291243bdc35ba93b
1ahmadsam <samiahmad360@hotmail.com>
2020-10-20 04:56:10 -0400

    added firebase deployment

--- src/Chat.js
+++ src/Chat.js
@@ -32,14 +32,18 @@ const Chat = () => {
 
   const sendMessage = (e) => {
     e.preventDefault();
-    db.collection('chats').doc(chatId).collection('messages').add({
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      message: input,
-      uid: user.uid,
-      photo: user.photo,
-      email: user.email,
-      displayName: user.displayName,
-    });
+    if (chatName) {
+      db.collection('chats').doc(chatId).collection('messages').add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: input,
+        uid: user.uid,
+        photo: user.photo,
+        email: user.email,
+        displayName: user.displayName,
+      });
+    } else {
+      alert('Click on a chat or create a new server');
+    }
 
     setInput('');
   };
